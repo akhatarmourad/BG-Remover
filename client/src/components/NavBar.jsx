ca190e232466f18d181484ad4ba1b1c603531e32
akhatarmourad <mourad.akhatar01@gmail.com>
2024-10-25 16:26:24 +0100

    Configure Clerck for authentication

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,12 @@
 import { Link } from 'react-router-dom';
 import { assets } from '../assets/assets';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 export default function NavBar() {
+
+  const { openSignIn } = useClerk();
+  const { isSignedIn, user } = useUser();
+
   return (
     <div className='bg-white flex justify-between items-center mx-4 p-3 lg:mx-40 shadow-md rounded-lg my-6'>
         {/* Logo + Name */}
@@ -10,10 +15,16 @@ export default function NavBar() {
             <h1 className='font-bold text-xl bg-gradient-to-r from-indigo-600 to-sky-600 text-transparent bg-clip-text'>BG Remover</h1>
         </Link>
 
-        {/* Get Started */}
-        <button className="bg-indigo-500 text-white flex items-center gap-3 px-3 py-2 shadow-sm rounded-lg">
+        {/* Button & Profile */}
+        { isSignedIn ? (
+          <div>
+            <UserButton />
+          </div>
+        ) : (
+          <button onClick={() => openSignIn()} className="bg-indigo-500 text-white flex items-center gap-3 px-3 py-2 shadow-sm rounded-lg">
             Get Started <img src={assets.arrow_icon} alt={assets.arrow_icon} className='w-3 sm:w-4' />
-        </button>
+          </button>
+        )}
     </div>
   )
 }
